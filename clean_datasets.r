c51ecf5312abc976f3935d4cf41f285b19d92928
r3kaushi <r3kaushi@uwaterloo.ca>
2023-06-02 19:21:33 +0200

    cleanup 1.0

---
+++ clean_datasets.r
@@ -0,0 +1,6 @@
+biden_dataset = read.csv("resources/joebiden.csv")  # path to joebiden dataset
+trump_dataset = read.csv("archive/donaldtrump.csv") # path to donaldtrump dataset
+
+# Removing rows with garbage values
+biden_dataset = biden_dataset[!is.na(as.POSIXct(as.character(biden_dataset$created_at), format = "%Y-%m-%d %H:%M:%S")),]
+trump_dataset = trump_dataset[!is.na(as.POSIXct(as.character(trump_dataset$created_at), format = "%Y-%m-%d %H:%M:%S")),]
