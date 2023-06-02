313e68428a7b3fce740c2cb6074ffbbde4add3e8
r3kaushi <r3kaushi@uwaterloo.ca>
2023-06-02 19:21:55 +0200

    cleanup 2.0

--- clean_datasets.r
+++ clean_datasets.r
@@ -4,3 +4,7 @@ trump_dataset = read.csv("archive/donaldtrump.csv") # path to donaldtrump datase
 # Removing rows with garbage values
 biden_dataset = biden_dataset[!is.na(as.POSIXct(as.character(biden_dataset$created_at), format = "%Y-%m-%d %H:%M:%S")),]
 trump_dataset = trump_dataset[!is.na(as.POSIXct(as.character(trump_dataset$created_at), format = "%Y-%m-%d %H:%M:%S")),]
+
+# Duplicate removal
+biden_dataset = biden_dataset %>% distinct(tweet_id, .keep_all = TRUE)
+trump_dataset = trump_dataset %>% distinct(tweet_id, .keep_all = TRUE)
